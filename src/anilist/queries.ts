d6147ed357d3d2e224c383afb481496681e49edf
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-06-25 20:02:04 +0600

    Add advanced search queries and update other queries with tags

--- src/anilist/queries.ts
+++ src/anilist/queries.ts
@@ -316,6 +316,17 @@ export const popularThisSeasonQuery = `query ($page: Int, $isAdult: Boolean = fa
           }
         }
         type
+        tags {
+          id
+          name
+          description
+          category
+          rank
+          isGeneralSpoiler
+          isMediaSpoiler
+          isAdult
+          userId
+        }
         startDate {
           year
           month
@@ -360,6 +371,17 @@ export const upcomingNextSeason = `query Query($season: MediaSeason, $seasonYear
           isAdult
           genres
           format
+          tags {
+          id
+          name
+          description
+          category
+          rank
+          isGeneralSpoiler
+          isMediaSpoiler
+          isAdult
+          userId
+        }
           studios(isMain: $isMain) {
             edges {
               isMain
@@ -427,6 +449,17 @@ export const top100Anime = `query ($page: Int, $perPage: Int, $sort: [MediaSort]
       isAdult
       meanScore
       popularity
+      tags {
+          id
+          name
+          description
+          category
+          rank
+          isGeneralSpoiler
+          isMediaSpoiler
+          isAdult
+          userId
+        }
       nextAiringEpisode {
         id
         airingAt
@@ -472,3 +505,154 @@ export const top100Anime = `query ($page: Int, $perPage: Int, $sort: [MediaSort]
     }
   }
 }`;
+
+export const advancedSearchQuery = `
+query (
+  $page: Int,
+  $id: Int,
+  $type: MediaType,
+  $isAdult: Boolean = false,
+  $search: String,
+  $format: [MediaFormat],
+  $status: MediaStatus,
+  $size: Int,
+  $countryOfOrigin: CountryCode,
+  $source: MediaSource,
+  $season: MediaSeason,
+  $seasonYear: Int,
+  $year: String,
+  $onList: Boolean,
+  $yearLesser: FuzzyDateInt,
+  $yearGreater: FuzzyDateInt,
+  $episodeLesser: Int,
+  $episodeGreater: Int,
+  $durationLesser: Int,
+  $durationGreater: Int,
+  $chapterLesser: Int,
+  $chapterGreater: Int,
+  $volumeLesser: Int,
+  $volumeGreater: Int,
+  $licensedBy: [String],
+  $isLicensed: Boolean,
+  $genres: [String],
+  $excludedGenres: [String],
+  $tags: [String],
+  $excludedTags: [String],
+  $minimumTagRank: Int,
+  $sort: [MediaSort] = [POPULARITY_DESC, SCORE_DESC]
+) {
+  Page(page: $page, perPage: $size) {
+    pageInfo {
+      total
+      perPage
+      currentPage
+      lastPage
+      hasNextPage
+    }
+    media(
+      id: $id,
+      type: $type,
+      season: $season,
+      format_in: $format,
+      status: $status,
+      countryOfOrigin: $countryOfOrigin,
+      source: $source,
+      search: $search,
+      onList: $onList,
+      seasonYear: $seasonYear,
+      startDate_like: $year,
+      startDate_lesser: $yearLesser,
+      startDate_greater: $yearGreater,
+      episodes_lesser: $episodeLesser,
+      episodes_greater: $episodeGreater,
+      duration_lesser: $durationLesser,
+      duration_greater: $durationGreater,
+      chapters_lesser: $chapterLesser,
+      chapters_greater: $chapterGreater,
+      volumes_lesser: $volumeLesser,
+      volumes_greater: $volumeGreater,
+      licensedBy_in: $licensedBy,
+      isLicensed: $isLicensed,
+      genre_in: $genres,
+      genre_not_in: $excludedGenres,
+      tag_in: $tags,
+      tag_not_in: $excludedTags,
+      minimumTagRank: $minimumTagRank,
+      sort: $sort,
+      isAdult: $isAdult
+    ) {
+      id
+      idMal
+      status(version: 2)
+      title {
+        userPreferred
+        romaji
+        english
+        native
+      }
+      bannerImage
+      coverImage {
+        extraLarge
+        large
+        medium
+        color
+      }
+      episodes
+      season
+      popularity
+      description
+      format
+      seasonYear
+      genres
+      averageScore
+      countryOfOrigin
+      nextAiringEpisode {
+        airingAt
+        timeUntilAiring
+        episode
+      }
+      studios {
+        edges {
+          isMain
+          node {
+            favourites
+            id
+            isAnimationStudio
+            isFavourite
+            name
+            siteUrl
+          }
+          id
+        }
+      }
+      startDate {
+        year
+        month
+        day
+      }
+      updatedAt
+      type
+      trending
+      synonyms
+      tags {
+          id
+          name
+          description
+          category
+          rank
+          isGeneralSpoiler
+          isMediaSpoiler
+          isAdult
+          userId
+        }
+      meanScore
+      duration
+      endDate {
+        year
+        month
+        day
+      }
+    }
+  }
+}
+`;
